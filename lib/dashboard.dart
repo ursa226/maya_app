ad9a97b2ca578af8e607c67cd80312028a04dc3b
marvin <ursa226@gmail.com>
2023-04-23 20:46:15 +0800

    just add chatsPage.dart

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -14,7 +14,7 @@ import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:maya_app/friends.dart';
-
+import 'package:maya_app/chatsPage.dart';
 class MyDash extends StatelessWidget {
   const MyDash({super.key});
 
@@ -144,123 +144,7 @@ class ChatUser {
   ChatUser({required this.name, required this.messageText, required this.imageURL, required this.time});
 }
 
-class ChatsScreen extends StatelessWidget {
-  const ChatsScreen({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          // Add the app bar
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Builder(
-                    builder: (BuildContext context) {
-                      return IconButton(
-                        icon: const Icon(Icons.menu),
-                        onPressed: () {
-                          Scaffold.of(context).openDrawer();
-                        },
-                      );
-                    },
-                  ),
-                  const Text(
-                    "Chats",
-                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          // Add the search box
-          Padding(
-            padding: const EdgeInsets.only(top: 16,left: 16,right: 16),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Search...",
-                hintStyle: TextStyle(color: Colors.grey.shade600),
-                prefixIcon: Icon(Icons.search,color: Colors.grey.shade600, size: 20,),
-                filled: true,
-                fillColor: Colors.grey.shade100,
-                contentPadding: const EdgeInsets.all(8),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                        color: Colors.grey.shade100
-                    )
-                ),
-              ),
-            ),
-          ),
-
-          // Add the conversation list
-          Expanded(
-            child: ListView.builder(
-              itemCount: chatUsers.length,
-              shrinkWrap: true,
-              padding: const EdgeInsets.only(top: 16),
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (context, index){
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const ChatDetailPage()));
-                  },
-                  child: ConversationList(
-                    name: chatUsers[index].name,
-                    messageText: chatUsers[index].messageText,
-                    imageUrl: chatUsers[index].imageURL,
-                    time: chatUsers[index].time,
-                    isMessageRead: (index == 0 || index == 3) ? true : false,
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the UI based on the selection.
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the UI based on the selection.
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-
-
-  }
-}
 
 
 // class SearchScreen extends StatelessWidget {
